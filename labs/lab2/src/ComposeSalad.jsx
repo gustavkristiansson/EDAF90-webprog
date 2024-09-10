1f9b6e5afe4b1e3bfe15a391bea1e1e63e589dfb
Gustav Kristiansson <70133787+gustavkristiansson@users.noreply.github.com>
2024-09-10 16:18:00 +0200

    Changed to className and htmlFor

--- labs/lab2/src/ComposeSalad.jsx
+++ labs/lab2/src/ComposeSalad.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function ComposeSalad(props) {
-  const foundationList = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
-  const [foundation, setFoundation] = useState('Pasta');
+  const foundationList = Object.keys(props.inventory).filter((name) => props.inventory[name].foundation);
+  const [foundation, setFoundation] = useState("Pasta");
   const [extras, setExtra] = useState({ Bacon: true, Fetaost: true });
 
   return (
-    <div className="continer col-12">
+    <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
         <h2>Välj innehållet i din sallad</h2>
-        <fieldset class="col-md-12">
-          <label for="foundation" class="form-label">Välj bas</label>
-          <select value="Sallad" class="form-select" id="foundation">
+        <fieldset className="col-md-12">
+          <label htmlFor="foundation" className="form-label">Välj bas</label>
+          <select value={foundation} className="form-select" id="foundation">
             <option key="Sallad" value="Sallad">Salad</option>
             <option key="Pasta" value="Pasta">Pasta</option>
           </select>
         </fieldset>
-
       </div>
     </div>
   );
