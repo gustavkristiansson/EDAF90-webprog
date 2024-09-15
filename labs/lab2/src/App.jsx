d6f30d7cf4a6f1f8dfac816e74bc1c6819e42031
Gustav Kristiansson <70133787+gustavkristiansson@users.noreply.github.com>
2024-09-15 17:27:19 +0200

    Continued work on lab2

--- labs/lab2/src/App.jsx
+++ labs/lab2/src/App.jsx
@@ -1,23 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import inventory from './inventory.mjs';
 import ComposeSalad from './ComposeSalad';
+import { useState } from 'react';
 
 function App() {
-  let extras = Object.keys(inventory).filter(name => inventory[name].extra);
+  const [shoppingCart, setShoppingCart] = useState([]);
+
   return (
     <div className="container py-4">
       <header className="pb-3 mb-4 border-bottom">
         <span className="fs-4">Min egen salladsbar</span>
       </header>
 
-      <div className="container col-12">
-        <div className="row h-200 p-5 bg-light border rounded-3">
-          <h2>Välj innehållet i din sallad</h2>
-          {extras.map(name => <div key={name} className="col-4">{name}</div>)}
-        </div>
-      </div>
+      <ComposeSalad inventory={inventory} addSalad={salad => setShoppingCart(shoppingCart.concat([salad]))}/>
 
-      <ComposeSalad inventory={inventory}></ComposeSalad>
+      {console.log(shoppingCart)}
 
       <footer className="pt-3 mt-4 text-muted border-top">
         EDAF90 - webprogrammering
--- labs/lab2/src/ComposeSalad.jsx
+++ labs/lab2/src/ComposeSalad.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import Salad from './Salad';
 
 function ComposeSalad(props) {
   const foundationList = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
   const proteinList = Object.keys(props.inventory).filter(name => props.inventory[name].protein);
   const saladExtras = Object.keys(props.inventory).filter(name => props.inventory[name].extra);
   const dressingList = Object.keys(props.inventory).filter(name => props.inventory[name].dressing);
-  const [foundation, setFoundation] = useState("Pasta");
-  const [protein, setProtein] = useState("Kycklingfilé");
-  // const [extras, setExtra] = useState({ Bacon: true, Fetaost: true });
-  const [extras, setExtra] = useState(saladExtras);
-  const [dressing, setDressing] = useState(dressingList)
+
+  const [foundation, setFoundation] = useState('Pasta');
+  const [protein, setProtein] = useState('Kycklingfilé');
+  const [extras, setExtra] = useState({ Bacon: true, Fetaost: true });
+  const [dressing, setDressing] = useState('Ceasardressing')
 
   function handleFoundation(event) {
     setFoundation(event.target.value);
@@ -19,41 +20,60 @@ function ComposeSalad(props) {
     setProtein(event.target.value);
   }
 
-  function handleExtras(event) {
-    setExtra(event.target.value);
-  }
-
   function handleDressing(event) {
     setDressing(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    props.addSalad(new Salad({
+      foundation: { name: foundation, ...props.inventory[foundation]},
+      protein: { name: protein, ...props.inventory[protein]},
+      dressing: { name: dressing, ...props.inventory[dressing]},
+      ...Object
+    }))
+
   }
 
   return (
     <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
         <h2>Välj innehållet i din sallad</h2>
-        <fieldset className="col-md-12">
-          <label htmlFor="foundation" className="form-label">Välj bas</label>
-          <select onChange={handleFoundation} value={foundation} className="form-select" id="foundation">
-            {foundationList.map(item => <option key={item} value={item}>{item}</option>)}
-          </select>
-          <label htmlFor="protein" className="form-label">Välj protein</label>
-          <select onChange={handleProtein} value={protein} className="form-select" id="protein">
-            {proteinList.map(item => <option key={item} value={item}>{item}</option>)}
-          </select>
-          <label htmlFor="extras" className="form-label">Välj tillbehör</label>
-          <div onChange={handleExtras} checked={extras} className="form-check" id="extras">
-            {saladExtras.map(item =>
-              <div key={item}>
-                <input className="form-check-input" type="checkbox" id="flexCheckDefault" />
-                <label className="form-check-label" htmlFor="flexCheckDefault">{item}</label>
-              </div>
-            )}
-          </div>
-          <label htmlFor="dressing" className="form-label">Välj dressing</label>
-          <select onChange={handleDressing} value={dressing} className="form-select" id="dressing">
-            {dressingList.map(item => <option key={item} value={item}>{item}</option>)}
-          </select>
-        </fieldset>
+        <form onSubmit={handleSubmit}>
+          <fieldset className="col-md-12">
+            <label htmlFor="foundation" className="form-label">Välj bas</label>
+            <select onChange={handleFoundation} value={foundation} className="form-select mb-3" id="foundation">
+              {foundationList.map(item => <option key={item} value={item}>{item}</option>)}
+            </select>
+            <label htmlFor="protein" className="form-label">Välj protein</label>
+            <select onChange={handleProtein} value={protein} className="form-select mb-3" id="protein">
+              {proteinList.map(item => <option key={item} value={item}>{item}</option>)}
+            </select>
+            <label htmlFor="extras" className="form-label">Välj tillbehör</label>
+            <div className="row row-cols-4 mb-3" id="extras">
+              {saladExtras.map(item =>
+                <div key={item} className="form-check">
+                  <input 
+                    onChange={event => setExtra({ ...extras, [item]: event.target.checked})} 
+                    className="form-check-input col" 
+                    type="checkbox" 
+                    id="flexCheckDefault" 
+                    value={item} 
+                    checked={extras[item] || false} 
+                  />
+                  <label className="form-check-label" htmlFor="flexCheckDefault">{item}</label>
+                </div>
+              )}
+            </div>
+            <label htmlFor="dressing" className="form-label">Välj dressing</label>
+            <select onChange={handleDressing} value={dressing} className="form-select" id="dressing">
+              {dressingList.map(item => <option key={item} value={item}>{item}</option>)}
+            </select>
+
+            <button type="submit" className="btn btn-primary mt-4">Beställ</button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
