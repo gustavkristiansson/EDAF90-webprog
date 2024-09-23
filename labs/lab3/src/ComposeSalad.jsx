54abac7d3d935e67d1e82eb5eb16d1da853e6c5b
elnpts <ellen.petersen98@gmail.com>
2024-09-23 12:07:37 +0200

    error handling

--- labs/lab3/src/ComposeSalad.jsx
+++ labs/lab3/src/ComposeSalad.jsx
@@ -15,10 +15,11 @@ function ComposeSalad(props) {
     (name) => props.inventory[name].dressing
   );
 
-  const [foundation, setFoundation] = useState("Pasta");
-  const [protein, setProtein] = useState("Kycklingfilé");
+  const [foundation, setFoundation] = useState("");
+  const [protein, setProtein] = useState("");
   const [extras, setExtra] = useState({});
-  const [dressing, setDressing] = useState("Ceasardressing");
+  const [dressing, setDressing] = useState("");
+  const [touched, setTouched] = useState(false);
 
   function handleFoundation(event) {
     setFoundation(event.target.value);
@@ -35,6 +36,11 @@ function ComposeSalad(props) {
   function handleSubmit(event) {
     event.preventDefault();
 
+    setTouched(true);
+    if (!event.target.checkValidity()) {
+      return;
+    }
+
     let salad = new Salad();
     salad
       .add(foundation, props.inventory[foundation])
@@ -46,42 +52,51 @@ function ComposeSalad(props) {
 
     props.addSalad(salad);
 
-    setFoundation("Pasta");
-    setProtein("Kycklingfilé");
-    setDressing("Ceasardressing");
+    setFoundation("");
+    setProtein("");
+    setDressing("");
     setExtra({});
+    setTouched(false);
   }
 
   return (
     <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
         <h2>Välj innehållet i din sallad</h2>
-        <form onSubmit={handleSubmit}>
+        <form
+          className={touched ? "was-validated" : ""}
+          noValidate
+          onSubmit={handleSubmit}>
           <fieldset className="col-md-12">
             <label htmlFor="foundation" className="form-label mt-4">
               Välj bas
             </label>
             <select
+              required
               onChange={handleFoundation}
               value={foundation}
               className="form-select mb-3"
               id="foundation"
             >
+              <option value="">Gör ditt val</option>
               {foundationList.map((item) => (
                 <option key={item} value={item}>
                   {item}
                 </option>
               ))}
             </select>
+            {/* <div className="invalid-feedback">Du måste fylla i</div> */}
             <label htmlFor="protein" className="form-label">
               Välj protein
             </label>
             <select
+              required
               onChange={handleProtein}
               value={protein}
               className="form-select mb-3"
               id="protein"
             >
+              <option value="">Gör ditt val</option>
               {proteinList.map((item) => (
                 <option key={item} value={item}>
                   {item}
@@ -117,11 +132,13 @@ function ComposeSalad(props) {
               Välj dressing
             </label>
             <select
+              required
               onChange={handleDressing}
               value={dressing}
               className="form-select"
               id="dressing"
             >
+              <option value="">Gör ditt val</option>
               {dressingList.map((item) => (
                 <option key={item} value={item}>
                   {item}
